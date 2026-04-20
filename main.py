9b5704f9c4b1316db10756b7d223e3582d9fabaa
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 23:46:59 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,12 @@
+def sekundga_o_tkaz(son):
+    return son / (60 * 60 * 24 * 365.25)
+```
+
+```python
+def sekundga_o_tkaz(son):
+    return son / 31536000
+```
+
+```python
+def sekundga_o_tkaz(son):
+    return son / (24 * 60 * 60)
